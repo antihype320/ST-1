b7b752c4a670c632f83e15d8cc77083366e46b40
antihype320 <132342878+antihype320@users.noreply.github.com>
2025-05-11 20:44:55 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -15,3 +15,54 @@ TEST(st1, sumPrime2) {
   uint64_t expected = 17;
   EXPECT_EQ(expected, res);
 }
+
+TEST(st1, sumPrime3) {
+    uint64_t res = sumPrime(30);
+    uint64_t expected = 129;
+    EXPECT_EQ(expected, res);
+}
+
+TEST(st2, checkPrime1) {
+    bool res = checkPrime(3);
+    EXPECT_TRUE(res);
+}
+
+TEST(st2, checkPrime2) {
+    bool res = checkPrime(17);
+    EXPECT_TRUE(res);
+}
+
+TEST(st2, checkPrime3) {
+    bool res = checkPrime(25);
+    EXPECT_FALSE(res);
+}
+
+TEST(st3, nPrime1) {
+    uint64_t res = nPrime(3);
+    uint64_t expected = 5;
+    EXPECT_EQ(expected, res);
+}
+
+TEST(st3, nPrime2) {
+    uint64_t res = nPrime(7);
+    uint64_t expected = 17;
+    EXPECT_EQ(expected, res);
+}
+
+TEST(st3, nPrime3) {
+    uint64_t res = nPrime(12);
+    uint64_t expected = 37;
+    EXPECT_EQ(expected, res);
+}
+
+TEST(st4, nextPrime1) {
+    uint64_t res = nextPrime(14);
+    uint64_t expected = 17;
+    EXPECT_EQ(expected, res);
+}
+
+TEST(st4, nextPrime2) {
+    uint64_t res = nextPrime(37);
+    uint64_t expected = 41;
+    EXPECT_EQ(expected, res);
+}
